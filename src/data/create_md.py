e62618281dce47c433da3cbbf457a503bc6d54a8
nathaneastwood <nathan.eastwood@icloud.com>
2024-07-23 17:28:51 +0100

    fix: Merge Nuu's teahouse locations

--- src/data/create_md.py
+++ src/data/create_md.py
@@ -1,4 +1,5 @@
 import re
+
 import numpy as np
 import pandas as pd
 from py_markdown_table.markdown_table import markdown_table
@@ -6,7 +7,7 @@ from py_markdown_table.markdown_table import markdown_table
 
 def create_md_file(file: str, sort: str):
     df = pd.read_csv(file, delimiter="|").sort_values(sort).replace({np.nan: ""})
-    m = markdown_table(df.to_dict(orient="records")).setParams(row_sep="markdown", quote=False).get_markdown()
+    m = markdown_table(df.to_dict(orient="records")).set_params(row_sep="markdown", quote=False).get_markdown()
     out = re.sub("csv", "md", file)
     f = open(out, "w")
     f.write("<!-- ### NOTE: This file should not be edited by hand. Please edit the .csv file. -->\n")
