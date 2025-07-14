ddc8adab82c3e5a62d5661e64868a82e70b249fd
Deploy from CI <>
2025-07-14 07:05:01 +0000

    Deploy d4c3be0004ea760b11ae4e155240b51bac5ab972 to gh-pages

---
+++ data/create_md.py
@@ -0,0 +1,23 @@
+import re
+
+import numpy as np
+import pandas as pd
+from py_markdown_table.markdown_table import markdown_table
+
+
+def create_md_file(file: str, sort: str):
+    df = pd.read_csv(file, delimiter="|").sort_values(sort).replace({np.nan: ""})
+    m = markdown_table(df.to_dict(orient="records")).set_params(row_sep="markdown", quote=False).get_markdown()
+    out = re.sub("csv", "md", file)
+    f = open(out, "w")
+    f.write("<!-- ### NOTE: This file should not be edited by hand. Please edit the .csv file. -->\n")
+    f.write(m)
+    f.close()
+
+
+create_md_file("./src/data/fauna.csv", "Name")
+create_md_file("./src/data/flora.csv", "Name")
+create_md_file("./src/data/characters.csv", "Name")
+create_md_file("./src/data/food-and-drink.csv", "Name")
+create_md_file("./src/data/locations.csv", "Name")
+create_md_file("./src/data/monsters.csv", "Name")
